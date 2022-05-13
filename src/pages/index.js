fb271661803e2b67f5cd074adb5e81f7d4a564fd
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2022-05-13 18:02:06 +0300

    [part-02] Set up a Gatsby site with bare minimums

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+// src/pages/index.js
+
+import React from 'react';
+
+const Index = (props) => {
+  return (
+    <div>
+      <h1>Hello Gatsby!</h1>
+      <p>(with bare minimums)</p>
+    </div>
+  )
+}
+
+export default Index
