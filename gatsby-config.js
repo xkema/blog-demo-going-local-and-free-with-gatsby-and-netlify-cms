fadfde692dcdefd0ff4cd38a3b5e54f0543017bb
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2022-05-15 14:52:22 +0300

    [part-04] Added source and transformer plugins for the pages and content
    - "gatsby-source-filesystem"
    - "gatsby-transformer-remark"

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,14 @@
 
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/content/`,
+        name: `content`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-netlify-cms`
   ]
 }
